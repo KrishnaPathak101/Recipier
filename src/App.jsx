9ace45d59cab4fc5b18c77980ae80f8b32d42067
Krishna-Ji <pathakkare@gmail.com>
2024-06-10 18:49:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "./Components/Sidebar"
+import Homepage from "./Pages/Homepage"
+
+
+function App() {
+
+  return (
+    <>
+      <div className=" flex">
+        <Sidebar />
+        <Homepage />
+      </div>
+
+    </>
+  )
+}
+
+export default App
